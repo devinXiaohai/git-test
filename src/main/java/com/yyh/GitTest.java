178ec3ab3841a6108d74db89dece646eedee9db3
yyh <1835759984@qq.com>
2022-05-16 19:45:40 +0800

    first commit

---
+++ src/main/java/com/yyh/GitTest.java
@@ -0,0 +1,14 @@
+package com.yyh;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author 8618042674353
+ * @Date 2022/5/16 19:42
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
